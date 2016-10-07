9ba4ab6f7e12249a376e9b545363c39b12239e62
Barış Esen <barisesenn@gmail.com>
2016-10-07 17:15:22 +0300

    init

---
+++ spec/hello_world_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe HelloWorld do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/hello_world.cr
@@ -0,0 +1,6 @@
+require "./hello_world/*"
+
+module HelloWorld
+  # TODO Put your code here
+  puts "hello world"
+end
